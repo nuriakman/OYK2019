48bd36222f31f1615613ca5d684b9a34415086e9
Nuri Akman <nuriakman@gmail.com>
2019-07-23 11:38:40 +0300

    Dosyadan okuma çaklışması

---
+++ sozler.dosyadan.php
@@ -0,0 +1,14 @@
+<h1>Sözler (Dosyadan)</h1>
+<?php
+
+$dosya = "sozler.txt";
+
+echo "<h2>file_get_contents:</h2>";
+$dosya_icerigi = file_get_contents($dosya);
+echo $dosya_icerigi;
+
+echo "<h2>file:</h2>";
+$dosya_icerigi = file($dosya);
+foreach ($dosya_icerigi as $key => $value) {
+  echo "<b>$key</b> elemanının değeri: <b>$value</b> <br />";
+}
