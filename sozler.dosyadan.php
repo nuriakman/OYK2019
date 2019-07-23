97dab9aeda5596501ed77def7d4b02f870e69f08
Nuri Akman <nuriakman@gmail.com>
2019-07-23 11:48:58 +0300

    Mesaj eklendi

--- sozler.dosyadan.php
+++ sozler.dosyadan.php
@@ -7,7 +7,7 @@ echo "<h2>file_get_contents:</h2>";
 $dosya_icerigi = file_get_contents($dosya);
 echo $dosya_icerigi;
 
-echo "<h2>file:</h2>";
+echo "<h2>file Komutu Çıktısı:</h2>";
 $dosya_icerigi = file($dosya);
 foreach ($dosya_icerigi as $key => $value) {
   echo "<b>$key</b> elemanının değeri: <b>$value</b> <br />";
