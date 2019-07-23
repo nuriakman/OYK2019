f63a5b57de866becba28807bfe6c64eb33d84a8e
Nuri Akman <nuriakman@gmail.com>
2019-07-23 12:12:54 +0300

    Ara yoldayız

--- kisiler.php
+++ kisiler.php
@@ -1,11 +1,21 @@
-<h1>.TXT Dosyasından Okunan Kişiler</h1>
+<h1>TXT Dosyasından Okunan Kişiler</h1>
 <?php
+
+$str = "Hasan;Çiçek;İstanbul";
+$arrKisi = explode(";", $str);
+
+echo "ADI: {$arrKisi[0]}<br />";
+echo "SOYADI: {$arrKisi[1]}<br />";
+echo "ŞEHİR: {$arrKisi[2]}<br />";
+
+die("");
 
 $dosya = "kisiler.txt";
 
 $arrSatirlar = file($dosya);
 
-print_r($arrSatirlar);
+print_r($arrSatirlar[1]);
+
 
 ?>
 <table border="1" cellpadding="5" cellspacing="0">
