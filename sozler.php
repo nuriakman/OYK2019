410a8012a1a5a15c458654ac32b8e0b9f43ec48d
Nuri Akman <nuriakman@gmail.com>
2019-07-23 14:49:22 +0300

    Count eklendi

--- sozler.php
+++ sozler.php
@@ -3,10 +3,11 @@
 
 $sozler = array("Bir elin nesi var",
                 "Birimiz hepimiz için",
+                "Damlaya damlaya",
                 "Saman altından su yürütmek",
                 "Sora sora bağdat bulunur");
 
 for($i=1; $i<=10; $i++) {
-  $SozNo = rand(0, 3);
+  $SozNo = rand(0, count($sozler)-1);
   echo $sozler[ $SozNo ] . "<br />";
 }
