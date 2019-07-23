2c83debe1373d501bbfe92c0460631473f702cbc
Nuri Akman <nuriakman@gmail.com>
2019-07-23 16:51:59 +0300

    Başladık

---
+++ text.dosyaya.yazma.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="tr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Text Dosyaya Yazma</title>
+  </head>
+  <body>
+
+    <h1>Text Dosyaya Yazma</h1>
+
+    <form method="post">
+      <p>
+        Ad Giriniz <input type="text" name="ad" />
+      </p>
+      <p>
+        Soyad Giriniz <input type="text" name="soyad">
+      </p>
+      <p>
+        <input type="submit" value="EKLE">
+      </p>
+    </form>
+
+
+  </body>
+</html>
