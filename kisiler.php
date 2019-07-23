c8163a9d9e35e0f7ae2945c4918e3a88b83b1a11
Nuri Akman <nuriakman@gmail.com>
2019-07-23 12:04:04 +0300

    Tablo eklendi

--- kisiler.php
+++ kisiler.php
@@ -7,6 +7,16 @@ $arrSatirlar = file($dosya);
 
 print_r($arrSatirlar);
 
-
-
 ?>
+<table border="1" cellpadding="5" cellspacing="0">
+  <tr>
+    <td> ADI </td>
+    <td> SOYADI </td>
+    <td> ŞEHİR </td>
+  </tr>
+  <tr>
+    <td>Nuri</td>
+    <td>AKMAN</td>
+    <td>ANKARA</td>
+  </tr>
+</table>
