eab86f96373dc03015e8a6eb9cc5d852cfe46a72
Nuri Akman <nuriakman@gmail.com>
2019-07-26 19:47:51 +0300

    Veritabanı işlemlerinden liste, silme, güncelleme tamamlandı.

--- db_islemleri/list.php
+++ db_islemleri/list.php
@@ -6,6 +6,7 @@
 
 <table border=1 cellpadding=5 cellspacing=0>
   <tr>
+    <td>KayıtNo</td>
     <td>ADI</td>
     <td>SOYADI</td>
     <td>ŞEHİR</td>
@@ -23,13 +24,14 @@
           <td>%s</td>
           <td>%s</td>
           <td>%s</td>
+          <td>%s</td>
 
           <td><a href='show.php?id=%s'>Göster</a></td>
           <td><a href='update.php?id=%s'>Güncelle</a></td>
           <td><a href='delete.php?id=%s'>Sil</a></td>
 
         </tr>",
-        $row["adi"], $row["soyadi"], $row["sehir"],
+        $row["id"], $row["adi"], $row["soyadi"], $row["sehir"],
         $row["id"], $row["id"], $row["id"]       );
   }
 
--- db_islemleri/update.php
+++ db_islemleri/update.php
@@ -1,6 +1,56 @@
 <?php
   include("db.php");
 
+  if( isset( $_POST["adi"]) ) {
+/*
+    // Formdan gelen veriler ile SQL komutu üretmek için bir yöntem:
+    $AD  = $_POST["adi"];
+    $SOY = $_POST["soyadi"];
+    $IL  = $_POST["sehir"];
+    $KAYITNO  = $_POST["id"];
+    $xyz = "
+    UPDATE db_deneme_rehber
+    SET
+      adi    = '$AD',
+      soyadi = '$SOY',
+      sehir  = '$IL'
+    WHERE id = '$KAYITNO' ";
+
+    // Formdan gelen veriler ile SQL komutu üretmek için diğer bir yöntem:
+    $xyz = "
+    UPDATE db_deneme_rehber
+    SET
+      adi    = '{$_POST["adi"]}',
+      soyadi = '{$_POST["soyadi"]}',
+      sehir  = '{$_POST["sehir"]}'
+    WHERE id = '{$_POST["sehir"]}' ";
+
+    // Formdan gelen veriler ile SQL komutu üretmek için başka bir yöntem:
+    $xyz = "
+    UPDATE db_deneme_rehber
+    SET
+      adi    = '" . $_POST["adi"] . "',
+      soyadi = '" . $_POST["soyadi"] . "',
+      sehir  = '" . $_POST["sehir"] . "'
+    WHERE id = '" . $_POST["id"] . "' ";
+*/
+
+    // GÜNCELLEME İŞLEMİMİZ BURADA
+    $xyz = sprintf("
+    UPDATE db_deneme_rehber
+    SET
+      adi    = '%s',
+      soyadi = '%s',
+      sehir  = '%s'
+    WHERE id = '%s' ",
+    $_POST["adi"], $_POST["soyadi"], $_POST["sehir"], $_POST["id"]);
+    //echo $xyz;  die();
+    $temp  = mysqli_query($db, $xyz);
+    echo "Kayıt güncelledi...";
+
+  }
+
+
   // Adres satırından gelen ID bilgisini al.
   $ID    = $_GET["id"];
 
@@ -22,7 +72,8 @@
 <h3>Kayıt Güncelleme</h3>
 
 <form method="post">
-  ID:<input type="text" name="id" value="<?php echo $id;?>">
+  ID: <?php echo $id;?>
+  <input type="hidden" name="id" value="<?php echo $id;?>">
   <br /><br />
   Adı:<input type="text" name="adi" value="<?php echo $adi;?>">
   <br /><br />
