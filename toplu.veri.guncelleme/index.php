3208422e6ad0079283141d893c91b010ab9f8ecd
Nuri Akman <nuriakman@gmail.com>
2019-07-29 16:39:57 +0300

    Yeni veri ekleme ve veri silme özelliği eklendi

--- toplu.veri.guncelleme/index.php
+++ toplu.veri.guncelleme/index.php
@@ -3,10 +3,14 @@
 ?>
 
 <?php
-  //echo "<pre>";  print_r($_POST);  echo "</pre>";
+  // echo "<pre>";  print_r($_POST);  echo "</pre>";
 
 
   if( isset($_POST["id"]) ) { // Form gönderilmişse başlayalım
+
+    ##########################
+    ########################## TOPLU VERİ GÜNCELLEME
+    ##########################
 
     // form elementinin adı [] ile bitiyorsa bu bir dizi değişkenidir
     //
@@ -38,7 +42,61 @@
               $FIYATI,
               $ID   );
 
+      if($FIYATI <> "SİL") {
+        // SQL Hazır, güncellemeyi yapalım
         $rows = mysqli_query($db, $SQL);
+      }
+
+      if($FIYATI == "SİL" or $FIYATI == "sil") {
+        // Silinmesi istenmiş
+        // SQL hazırlayalım
+        $SQL = "DELETE FROM yemek_menusu WHERE id = '$ID' ";
+        // ve kaydı silelim....
+        $rows = mysqli_query($db, $SQL);
+      }
+
+
+    } //foreach
+
+    // Buraya geldiğimizde, tüm kayıtlar güncellenmiştir.
+
+
+    ##########################
+    ########################## TOPLU VERİ EKLEME
+    ##########################
+
+    // form elementinin adı [] ile bitiyorsa bu bir dizi değişkenidir
+    //
+    // TOPLU VERİ EKLEME ADIMLARI
+    // foreach ile yeni_ID dizisi üzerinde dönülür.
+    // Döngünün $KEY'i esas alınarak diğer dizilerden veriler alınır
+    // SQL oluşturulur ve çalıştırılır
+
+    // Form değişkenlerini bir dizi değişkenine atadık
+    $arrID          = $_POST["yeni_id"];
+    $arrYemekTuru   = $_POST["yeni_yemek_turu"];
+    $arrYemekAdi    = $_POST["yeni_yemek_adi"];
+    $arrYemekFiyati = $_POST["yeni_yemek_fiyati"];
+
+    foreach ($arrID as $key => $value) {
+      $ID     = $arrID[$key];
+      $TURU   = $arrYemekTuru[$key];
+      $ADI    = $arrYemekAdi[$key];
+      $FIYATI = $arrYemekFiyati[$key];
+      $SQL = sprintf("INSERT INTO yemek_menusu
+              SET
+                  yemek_turu   = '%s',
+                  yemek_adi    = '%s',
+                  yemek_fiyati = '%s' ",
+              $TURU,
+              $ADI,
+              $FIYATI  );
+
+        // Eğer, yemek adı (yani $ADI değişkeni) DOLUYSA yeni kaydı ekleyelim...
+        if( trim($ADI) <> "") { // trim: boşlukları temizler
+          $rows = mysqli_query($db, $SQL);
+        }
+
     } //foreach
 
     // Buraya geldiğimizde, tüm kayıtlar güncellenmiştir.
@@ -94,6 +152,17 @@ while($row = mysqli_fetch_assoc($rows)) {
 
 } // while sonu
 
+// 5 Adet YENİ satırı ekler
+for($i=1; $i<=5; $i++) {
+  echo "
+      <tr>
+        <td>YENİ<input type='hidden' name='yeni_id[]'></td>
+        <td><input type='text' name='yeni_yemek_turu[]' placeholder='Yemek türü giriniz'/></td>
+        <td><input type='text' name='yeni_yemek_adi[]' placeholder='Yemek adı giriniz'/></td>
+        <td><input type='text' name='yeni_yemek_fiyati[]' placeholder='Yemek fiyatı giriniz'/></td>
+      </tr>";
+}
+
 echo $TabloSonu;
 
 
@@ -103,3 +172,5 @@ mysqli_close($db);
 
   <input type='submit' value='Kaydet !'>
 </form>
+
+<p>NOT: Silmek için FİYATI alanı içine SİL yazınız.  </p>
