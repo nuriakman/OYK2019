3e43727ae1f663a610d72c16798c8d08d6b1b083
Nuri Akman <nuriakman@gmail.com>
2019-08-01 22:04:36 +0300

    .gitignore güncellendi

---
+++ db_islemleri/db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username   = "root";
+$password   = "root";
+$dbname     = "bilgiler";
+
+// Veritabanı bağlantısının oluşturulması
+$db = mysqli_connect($servername, $username, $password, $dbname);
+// Varsa, bağlantı hatasının ekrana yazdırılarak programın sonlandırılması
+if (!$db) { die("Hata oluştu: " . mysqli_connect_error()); }
+//echo "Bağlantı tamam!";
+
+// Oluşabilecek Türkçe karakter gösterimi sorunlarını giderelim...
+mysqli_query($db, "set names 'utf8'");
+
+?>
